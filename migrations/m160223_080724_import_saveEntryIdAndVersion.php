32118b7b8da740464b3ac3043e27df9544b2371a
Jack McNicol <jackmcpickle@gmail.com>
2016-02-29 14:56:25 +1030

    Use sql directly to make work

--- migrations/m160223_080724_import_saveEntryIdAndVersion.php
+++ migrations/m160223_080724_import_saveEntryIdAndVersion.php
@@ -18,7 +18,7 @@ class m160223_080724_import_saveEntryIdAndVersion extends BaseMigration
         // Allow changing to 'Custom'
         $this->execute("ALTER TABLE `craft_formerly_questions` CHANGE `type` `type` ENUM('PlainText','MultilineText','Coded','Custom', 'Dropdown','RadioButtons','Checkboxes','Email','Tel','Url','Number','Date') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL");
         //Change 'Coded' to 'Custom'
-        $this->update('formerly_questions', array('type' => 'Custom'), 'type=:type', array(':type'=>'Coded'));
+        $this->execute("UPDATE `craft_formerly_questions` SET `type`='Custom' WHERE `type`='Coded'");
         //Remove the 'Coded' type
         $this->execute("ALTER TABLE `craft_formerly_questions` CHANGE `type` `type` ENUM('PlainText','MultilineText','Custom','Dropdown','RadioButtons','Checkboxes','Email','Tel','Url','Number','Date') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULt 'PlainText'");
 
